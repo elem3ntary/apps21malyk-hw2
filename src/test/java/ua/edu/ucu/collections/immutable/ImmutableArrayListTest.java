982146e965fb7536b53ca02a03e45c1c25e47a7e
Stefan <49642756+elem3ntary@users.noreply.github.com>
2021-12-12 12:11:06 +0200

    Make test public

--- src/test/java/ua/edu/ucu/collections/immutable/ImmutableArrayListTest.java
+++ src/test/java/ua/edu/ucu/collections/immutable/ImmutableArrayListTest.java
@@ -14,23 +14,23 @@ class ImmutableArrayListTest {
     private ImmutableArrayList immutableArrayList;
 
     @BeforeEach
-    void setUp() {
+    public void setUp() {
         immutableArrayList = new ImmutableArrayList();
     }
 
     @Test
-    void add() {
+    public void testAdd() {
         ImmutableArrayList testList =  immutableArrayList.add(1);
         assertEquals(testList.get(0), 1);
         testList =  testList.add(2);
         assertEquals(testList.get(1), 2);
     }
     @Test
-    void addWithIndexOutOfRange() {
+    public void testAddWithIndexOutOfRange() {
         assertThrows(IndexOutOfBoundsException.class, ()->immutableArrayList.add(0,1));
     }
     @Test
-    void addWithIndex() {
+    public void testAddWithIndex() {
         ImmutableArrayList testList = immutableArrayList.add(1);
         testList = testList.add(0,2);
         assertEquals(2, testList.get(0));
@@ -39,7 +39,7 @@ class ImmutableArrayListTest {
     }
 
     @Test
-    void addAll() {
+    public void testAddAll() {
         ImmutableArrayList testList = immutableArrayList.addAll(new Object[]{1,2,3});
         assertEquals(1, testList.get(0));
         assertEquals(2, testList.get(1));
@@ -48,7 +48,7 @@ class ImmutableArrayListTest {
     }
 
     @Test
-    void addAllWithIndex() {
+    public void testAddAllWithIndex() {
         ImmutableArrayList testList = immutableArrayList.add(1);
         testList = testList.addAll(0,new Object[]{1,2,3});
         assertEquals(1, testList.get(0));
@@ -59,17 +59,17 @@ class ImmutableArrayListTest {
 
 
     @Test
-    void get() {
+    public void testGet() {
         ImmutableArrayList testList = immutableArrayList.add(1);
         assertEquals(1, testList.get(0));
     }
     @Test
-    void getOutOfIndex() {
+    public void testGetOutOfIndex() {
         assertThrows(ArrayIndexOutOfBoundsException.class, () -> immutableArrayList.get(0));
     }
 
     @Test
-    void remove() {
+    public void testRemove() {
         ImmutableArrayList testList = immutableArrayList.add(1);
         ImmutableArrayList testList2 = testList.remove(0);
         assertNotSame(testList, testList2);
@@ -81,29 +81,29 @@ class ImmutableArrayListTest {
     }
 
     @Test
-    void set() {
+    public void testSet() {
         ImmutableArrayList testList = immutableArrayList.add(1).set(0, 2);
         assertEquals(2, testList.get(0));
     }
     @Test
-    void setOutOfBound() {
+    public void testSetOutOfBound() {
         ImmutableArrayList testList = immutableArrayList.add(1);
         // base = [1, null], length = 1
         assertThrows(IndexOutOfBoundsException.class, () -> testList.set(1, 2));
     }
 
     @Test
-    void indexOf() {
+    public void testIndexOf() {
         assertEquals(0, immutableArrayList.add(1).indexOf(1));
     }
     @Test
-    void indexOfNoElement() {
+    public void testIndexOfNoElement() {
         ImmutableArrayList testList = immutableArrayList.add(1);
         assertThrows(NoSuchElementException.class, () -> testList.indexOf(2));
     }
 
     @Test
-    void size() {
+    public void testSize() {
         ImmutableArrayList testList = immutableArrayList.add(1).add(2).addAll(new Object[]{3,4});
         assertEquals(4, testList.size());
         testList = testList.remove(0);
@@ -111,7 +111,7 @@ class ImmutableArrayListTest {
     }
 
     @Test
-    void clear() {
+    public void testClear() {
         ImmutableArrayList testList = immutableArrayList.add(1);
         testList.clear();
         assertEquals(0, testList.size());
@@ -120,7 +120,7 @@ class ImmutableArrayListTest {
 
 
     @Test
-    void toArray() {
+    public void testToArray() {
         Object[] toCompare = new Object[]{2,4,5,6,7};
         ImmutableArrayList testList = immutableArrayList.addAll(toCompare);
         assertArrayEquals(toCompare, testList.toArray());
